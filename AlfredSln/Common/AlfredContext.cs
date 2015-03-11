817c5ff74b83ae895e864dbb23248e5452e9939b
Lior Zamir <liorzam@gmail.com>
2015-03-11 11:59:05 +0200

    Entities

---
+++ AlfredSln/Common/AlfredContext.cs
@@ -0,0 +1,23 @@
+﻿using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class AlfredContext : DbContext
+    {
+        public AlfredContext()
+            : base("DefaultConnection")
+        {
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<MifgaimContext, MifgaimCivilians.Migrations.Configuration>("DefaultConnection"));
+        }
+
+        public DbSet<User> MyUsers { get; set; }
+        public DbSet<Expression> MyExpressions { get; set; }
+        public DbSet<Movie> MyMovies { get; set; }
+    }
+}
