4a77c47db5a6fe1771a82f746805dae1101084b5
DoresPoog <dorespoog@gmail.com>
2015-03-11 13:02:32 +0200

    re

--- AlfredSln/Common/Models/UserExperience.cs
+++ AlfredSln/Common/Models/UserExperience.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,19 @@ namespace Common.Models
 {
     public class UserExperience
     {
+
         public int MovieId { get; set; }
         public int UserId { get; set; }
         public int ExpressionId { get; set; }
         public int Precentage { get; set; }
 
-
+        [ForeignKey("MovieId")]
         public virtual Movie MyMovie {get;set;}
 
+        [ForeignKey("UserId")]
         public virtual User MyUser { get; set; }
 
+        [ForeignKey("MovieId")]
         public virtual Expression MyExpression { get; set; }
 
     }
